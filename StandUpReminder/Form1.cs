75d974b25dcd8005d6622113a684273a6fc0bf5c
Singgih Cahyono <scahyono@gmail.com>
2015-09-23 11:15:10 +0200

    interrupt full screen video

--- StandUpReminder/Form1.cs
+++ StandUpReminder/Form1.cs
@@ -12,9 +12,25 @@ using System.Windows.Forms;
 
 namespace StandUpReminder
 {
+    using System.Runtime.InteropServices;
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT { public int Left; public int Top; public int Right; public int Bottom;}
+
     public partial class Form1 : Form
     {
-
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetShellWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowRect(IntPtr hwnd, out RECT rc);
+        private IntPtr desktopHandle; //Window handle for the desktop    
+        private IntPtr shellHandle; //Window handle for the shell    
+        const int TIMER_MINUTES = 60;
+        const int WARNING_SECONDS = 3595;//60;
+        const int AUTOLOCK_SECONDS = 3590;//10
         DateTime lastLoginDate = DateTime.Now;
         bool allowExit = false;
         Color originalBackColor;
@@ -22,6 +38,31 @@ namespace StandUpReminder
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool IsThereFullScreen()         {
+            //Get the handles for the desktop and shell now.    
+            desktopHandle = GetDesktopWindow();    
+            shellHandle = GetShellWindow();
+            //Detect if the current app is running in full screen    
+            bool runningFullScreen = false; 
+            RECT appBounds; 
+            Rectangle screenBounds; 
+            IntPtr hWnd;    
+            //get the dimensions of the active window    
+            hWnd = GetForegroundWindow();    
+            if (hWnd!=null && !hWnd.Equals(IntPtr.Zero))    {        
+                //Check we haven't picked up the desktop or the shell        
+                if (!(hWnd.Equals(desktopHandle) || hWnd.Equals(shellHandle)))        {            
+                    GetWindowRect(hWnd, out appBounds);            
+                    //determine if window is fullscreen            
+                    screenBounds = Screen.FromHandle(hWnd).Bounds;            
+                    if ((appBounds.Bottom - appBounds.Top) == screenBounds.Height && (appBounds.Right - appBounds.Left) == screenBounds.Width)            {                
+                        runningFullScreen = true;            
+                    }        
+                }    
+            }
+            return runningFullScreen;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -34,7 +75,8 @@ namespace StandUpReminder
 
             RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             String value = (string)key.GetValue("StandUpReminder");
-            if (value == "dummy") {
+            if (value == "dummy")
+            {
                 key.SetValue("StandUpReminder", System.Reflection.Assembly.GetEntryAssembly().Location, RegistryValueKind.String);
             }
             key.Close();
@@ -47,35 +89,37 @@ namespace StandUpReminder
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            TimeSpan span = lastLoginDate.AddMinutes(60).Subtract(DateTime.Now);
+            TimeSpan span = lastLoginDate.AddMinutes(TIMER_MINUTES).Subtract(DateTime.Now);
             TimeSpan showSpan = span.Subtract(TimeSpan.FromMinutes(10));
             int totalSeconds = (int)span.TotalSeconds;
             progressBar1.Value = totalSeconds;
 
-            if (totalSeconds <= 0) {
-                Process.Start(@"C:\Windows\system32\rundll32.exe","user32.dll,LockWorkStation");
+            if (totalSeconds <= 0)
+            {
+                Process.Start(@"C:\Windows\system32\rundll32.exe", "user32.dll,LockWorkStation");
                 timer1.Enabled = false;
                 return;
             }
 
             String prefix = "";
 
-            if (totalSeconds > 600)
+            if (totalSeconds > WARNING_SECONDS)
             {
                 prefix = "";
             }
-            else if (totalSeconds > 60)
+            else if (totalSeconds > AUTOLOCK_SECONDS)
             {
                 progressBar1.BackColor = Color.Yellow;
                 prefix = "Please, lock Windows now and walk away! ";
                 Show();
                 showSpan = span;
-            } 
+            }
             else
             {
                 progressBar1.BackColor = Color.Red;
                 prefix = "Automatic lock imminent! ";
                 Show();
+                if (IsThereFullScreen()) Activate();
                 showSpan = span;
             }
             string timeLeft = showSpan.Minutes.ToString("00") + ":" + showSpan.Seconds.ToString("00");
