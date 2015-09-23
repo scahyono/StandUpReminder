68d948facb0702383078d22ef3ae30b19fcb7a43
Singgih Cahyono <scahyono@gmail.com>
2015-09-23 10:38:38 +0200

    detect starting twice #1

--- StandUpReminder/Program.cs
+++ StandUpReminder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,24 @@ namespace StandUpReminder
         [STAThread]
         static void Main()
         {
+            string procName = Process.GetCurrentProcess().ProcessName.Split('.')[0];
+
+            Process[] processes = Process.GetProcesses();
+            int count = 0;
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName.StartsWith(procName))
+                {
+                    count++;
+                }
+            }
+
+            if (count > 1)
+            {
+                MessageBox.Show(procName + " already running");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
